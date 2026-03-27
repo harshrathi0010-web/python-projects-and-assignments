793cd224a1576b411d6040b83c116173256818ed
harsh rathi <harshrathi0010@gmail.com>
2026-03-27 14:20:30 +0530

    loop and pattern printing assignments
    Updated comments and cleaned up code for loop and pattern printing assignments.

--- loop and pattern printing assignments.py
+++ loop and pattern printing assignments.py
@@ -1,5 +1,82 @@
 # loops and pattern printing assignments
 
+
+# break statement= It will take exit from the current loop
+
+##for i in range(1,6):
+##    if i==3:
+##        break
+##    print(i)
+
+##for i in range(1,6):
+##    print(i)
+##    if i==3:
+##        break
+
+# continue statement=continue will take the cursor to the next iteration
+#                               or
+# continue will go to the loop again
+
+##for i in range(1,6):
+##    if i==3:
+##        continue  # it will skip i==3 and continue to loop again
+##    print(i)
+
+##for i in range(1,6):
+##    print(i)
+##    if i==3:
+##        continue
+
+
+# pass statement= will do do thing and take the cursor to the next line
+##for i in range(1,6):
+##    if i==3:
+##        pass 
+##        print(i)
+
+
+##for i in range(1,6):
+##    print(i)
+##    if i==5:
+##        break 
+##else:
+##    print(0)
+
+##for i in range(1,9,2):
+##    if i%0b110==0:       #0b110 it is called binary notation  and also start with the 0b
+##                        # it is saying i%6==0:
+##        break
+##    print(i)
+
+
+##for i in range(1,10):
+##    print(i)
+##else:
+##    print(0) # else part will work when the loop working fully without any break
+
+##for i in range(1,5):
+##    if i==3:
+##        break
+##    print(i)    
+##else:
+##    print(0)    # here else did not work bcoz here the condtion is true(i==3).
+
+
+# nested for loop
+
+##n=int(input("enter the num:"))
+##if n<2:
+##    print("not prime num")
+##else:
+##    
+##    for i in range(2,n):
+##        if n%i==0:
+##            print("not prime num")
+##            break
+##    
+##    else:
+##        print("prime num")
+
 #ques. print number use a loop from 1-10
 
 ##for i in range(1,11):
@@ -115,38 +192,160 @@
 
 #  Pattern Questions
 
-##*
-##* *
-##* * *
-##* * * *
-##* * * * *
+# pattern printing
+#ques1.
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+
+##for i in range(1,6):
+##    for j in range(1,6):
+##        print("*",end=" ")    
+##    print()
+
+#ques2.
+#*
+#* *
+#* * *
+#* * * *
+#* * * * *
 
 ##for i in range(1,6):
 ##    for j in range(1,i+1):
-##        print("* ",end="")
+##        print("*",end=" ")
 ##    print()
 
-##* * * * *
-##* * * *
-##* * *
-##* *
-##*
+#ques3.
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
 
 ##for i in range(5,0,-1):
 ##    for j in range(1,i+1):
-##        print("* ",end="")
+##        print("*",end=" ")
 ##    print()
 
-##1 
-##12 
-##123 
-##1234 
-##12345
+#ques4.
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+##for i in range(1,6):
+##    for k in range(1,i+1):
+##        print(k,end=" ")
+##    print()
+
+#ques5.
+
+# 5 4 3 2 1
+# 4 3 2 1
+# 3 2 1
+# 2 1
+# 1
+
+##for i in range(5,0,-1):
+##    for j in range(i,0,-1):
+##        print(j,end=" ")
+##    print()
+
+
+##k=1
+##for i in range(1,5):
+##    for j in range(1,i+1):
+##        print(k, end= " ")
+##        k+=1
+##    print()
+
+#ques 6.
+##1
+##01
+##010
+##1010
+##10101
+
+
+##k=1
+##m=1
+##for i in range(1,6):
+##    for j in range(1,i+1):
+##        print(k,end="")
+##        k=k-m
+##        m=-m
+##    print()
+
+ #       OR
+##k=1
+##for i in range(1,6):
+##    for j in range(1,i+1):
+##        print(k%2,end="")
+##        k+=1
+##    print()
+
+#ques7.
+
+##A
+##AB
+##ABC
+##ABCD
+##ABCDE
+ 
+
+##for i in range(1,6):
+##    for j in range(1,i+1):   # we used 65+ for upper case and from 97 for lowercase
+##                             # and for the numeric we used from 48-57 for [0-9]
+##        print(chr(j+64),end="")
+##    print()
+
+#ques8.
+
+##A
+##BB
+##CCC
+##DDDD
+##EEEEE
 
 ##for i in range(1,6):
 ##    for j in range(1,i+1):
-##        print(j,end=" ")
+##        print(chr(i+64),end=" ")
 ##    print()
+
+#ques 9.
+
+##A
+##BC
+##DEF
+##GHIJ
+##KLMNO
+
+ 
+##k=65
+##for i in range(1,6):
+##    for j in range(1,i+1):
+##        print(chr(k),end="")
+##        k+=1
+##    print()
+
+#ques10.
+
+##    *
+##   **
+##  ***
+## ****
+##*****
+
+##for i in range(1,6):
+##    for k in range(5,i,-1):
+##        print(" ",end="")
+##    for j in range(1,i+1):
+##        print("*",end="")
+##    print()
+
 
 ##    *
 ##   * *
@@ -157,10 +356,30 @@
 ##for i in range(1,6):
 ##    for k in range(5,i,-1):
 ##        print(" ",end="")
-##        
+##    
 ##    for j in range(1,i+1):
 ##        print("* ",end="")
 ##    print()
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
 
 #Print a star pattern Stop printing when the row number reaches 4.
 
@@ -242,9 +461,148 @@
 ##    count+=1
 ##print(count)
 
-#Check whether a number is palindrome or not using while loop.
+# ques. WAP to add the number of a digit take number from the user with while loop
 
-#
+##num=int(input("enter the number:"))
+##add=0
+##while num>0:
+##    rem=num%10
+##    add=add+rem
+##    num=num//10
+##print(add)
+
+
+
+# ques .WAP to reverse a number
+
+##num=int(input("enter the num:"))
+##rev=0
+##while num>0:
+##    digit=num%10
+##    rev=rev*10+digit
+##    num=num//10
+##print("reversed number:",rev)
+
+# ques. WAP to check whether a number is palindrome or not.
+
+##num=int(input("enter the num:"))
+##original=num
+##rev=0
+##while num>0:
+##    digit=num%10
+##    rev=rev*10+digit
+##    num=num//10
+##if original==rev:
+##    print("number is palindrome")
+##else:
+##    print("number is not palindrome")
+
+# ques. Ask user to enter password until correct password is entered.
+##print("www.harsh.com")
+##password="harsh@"
+##while True:
+##    
+##    user_input=input("enter the password:")
+##
+##    if user_input==password:
+##        print(" access granted")
+##        break
+##    else:
+##        print("wrong password,try again")
+
+# ques. Create a number guessing game: 
+##• Generate a random number (1–10) 
+##• Keep asking user until they guess correctly
+
+
+##import random
+##num=random.randint(1,10)
+##while True:
+##    user_input=int(input("enter the num as user:"))
+##    if user_input==num:
+##        print("good number is matching")
+##        break
+##    else:
+##        print("number is not matching,try again")
+
+# ques .Keep taking input numbers until user enters 0, then print total sum.
+
+##sum=0
+##while True:
+##    num=int(input("enter the number:"))
+##    if num==0:
+##        break
+##    
+##    sum+=num
+##print(sum)
+
+# ques. Check whether a number is Armstrong number.
+##
+##num=int(input("enter the num:"))
+##original=num
+##digits=str(num)
+##n=len(digits)
+##total=0
+##while num>0:
+##    digit=num%10
+##    total+=digit**n
+##    num=num//10
+##if total==original:
+##    print("number is armstrong:",num)
+##    
+##else:
+##    print("not arm strong:",num)
+
+# ques. Print Fibonacci series up to N terms using while loop.
+##a=0
+##b=1
+##terms=10
+##for i in range(10):
+##    c=a+b
+##    print(c)
+##    a=b
+##    b=c
+
+
+
+
+#ques.Print prime numbers between 1 to 50 using while loop.
+
+##num=2
+##while num<=50:
+##  count=0
+##  i=2
+##while i<=num-1:
+##    if num%2==0:
+##            count+=1
+##     i+=1
+##   if count==0:
+##       print(num,end=" ")
+##  num+=1
+
+
+
+    
+            
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
